eef3e2492fc6c80d7165399ffb1dd7218cabb12a
DESKTOP-A659E8N\yin <123>
2025-08-12 10:40:58 +0800

    fix

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     uvicorn.run(
         "app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=8002,
         reload=True,
         log_level="info",
     )
